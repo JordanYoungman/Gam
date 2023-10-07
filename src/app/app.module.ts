ccf20b6be02e50d639eee795cbf54069ec959fdf
effie <havealittlefaith2096@gmail.com>
2023-10-07 22:14:12 +0100

    feature: user card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import {NgIf, NgFor} from '@angular/common';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {FormsModule} from '@angular/forms';
-import { HomeComponent } from './features/home/home.component';
-import { FriendsComponent } from './features/friends/friends.component';
-import { FeedComponent } from './features/feed/feed.component';
-import { ListsComponent } from './features/lists/lists.component';
-import { SettingsComponent } from './features/settings/settings.component';
-import { HttpClientModule } from '@angular/common/http';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { GameTileComponent } from './shared/game-tile/game-tile.component';
-import {MatCardModule} from '@angular/material/card';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { NgIf, NgFor } from '@angular/common'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatCheckboxModule } from '@angular/material/checkbox'
+import { FormsModule } from '@angular/forms'
+import { HomeComponent } from './features/home/home.component'
+import { FriendsComponent } from './features/friends/friends.component'
+import { FeedComponent } from './features/feed/feed.component'
+import { ListsComponent } from './features/lists/lists.component'
+import { SettingsComponent } from './features/settings/settings.component'
+import { HttpClientModule } from '@angular/common/http'
+import { MatGridListModule } from '@angular/material/grid-list'
+import { GameTileComponent } from './shared/game-tile/game-tile.component'
+import { MatCardModule } from '@angular/material/card'
+import { UserCardComponent } from './shared/user-card/user-card.component'
 
 @NgModule({
   declarations: [
@@ -29,7 +30,8 @@ import {MatCardModule} from '@angular/material/card';
     FeedComponent,
     ListsComponent,
     SettingsComponent,
-    GameTileComponent
+    GameTileComponent,
+    UserCardComponent,
   ],
   imports: [
     BrowserModule,
@@ -45,9 +47,9 @@ import {MatCardModule} from '@angular/material/card';
     FormsModule,
     HttpClientModule,
     MatGridListModule,
-    MatCardModule
+    MatCardModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
