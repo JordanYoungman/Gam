bee54848e1390b0ff8b35d0336e2945fc114a68c
effie <havealittlefaith2096@gmail.com>
2023-10-09 18:48:31 +0100

    feat(style): changed theme framework
    Removed material angular.
    Added material prime.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,7 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { MatIconModule } from '@angular/material/icon'
-import { MatButtonModule } from '@angular/material/button'
-import { MatToolbarModule } from '@angular/material/toolbar'
 import { NgIf, NgFor } from '@angular/common'
-import { MatSidenavModule } from '@angular/material/sidenav'
-import { MatCheckboxModule } from '@angular/material/checkbox'
 import { FormsModule } from '@angular/forms'
 import { HomeComponent } from './features/home/home.component'
 import { FriendsComponent } from './features/friends/friends.component'
@@ -17,10 +12,7 @@ import { FeedComponent } from './features/feed/feed.component'
 import { ListsComponent } from './features/lists/lists.component'
 import { SettingsComponent } from './features/settings/settings.component'
 import { HttpClientModule } from '@angular/common/http'
-import { MatGridListModule } from '@angular/material/grid-list'
 import { GameTileComponent } from './shared/game-tile/game-tile.component'
-import { MatCardModule } from '@angular/material/card'
-import { UserCardComponent } from './shared/user-card/user-card.component'
 
 @NgModule({
   declarations: [
@@ -31,25 +23,17 @@ import { UserCardComponent } from './shared/user-card/user-card.component'
     ListsComponent,
     SettingsComponent,
     GameTileComponent,
-    UserCardComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatIconModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatCheckboxModule,
     NgIf,
     NgFor,
     FormsModule,
     HttpClientModule,
-    MatGridListModule,
-    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
